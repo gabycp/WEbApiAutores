aadf99f0b1821a586689c127055243ca4f2fbbfc
gabycp <gaby_cor93@hotmail.com>
2023-01-05 16:09:19 -0600

    Crear clase para Middleware

---
+++ WEbApiAutores/Middlewares/LoguearRespuestaHTTPMiddleware.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace WEbApiAutores.Middlewares
+{
+
+    //Los metodos de excepcion solo pueden colocar en clases estaticas
+
+    public static class  LoguearRespuestaHTTPMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseLoguearRespuestaHTTP(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<LoguearRespuestaHTTPMiddleware>();
+        }
+    }
+    public class LoguearRespuestaHTTPMiddleware
+    {
+        private readonly RequestDelegate siguiente;
+        private readonly ILogger<LoguearRespuestaHTTPMiddleware> logger;
+
+        public LoguearRespuestaHTTPMiddleware(RequestDelegate siguiente, ILogger<LoguearRespuestaHTTPMiddleware> logger)
+        {
+            this.siguiente = siguiente;
+            this.logger = logger;
+        }
+
+        //Invoke o InvokeAsyn -- Retornar una tarea y aceptar  un paremetro httpContext
+
+        public async Task InvokeAsync(HttpContext contexto)
+        {
+            //Informacion brindad por Raul Liz
+            // Entrada del middleware
+
+            // 1. Creo un MemoryStream para poder manipular
+            // y copiarme el cuerpo de la respuesta.
+            // Esto se hace porque el stream del cuerpo de la
+            // respuesta no tiene permisos de lectura.
+            using var ms = new MemoryStream();
+
+            // 2. Guardo la referencia del Stream donde se
+            // escribe el cuerpo de la respuesta
+            var cuerpoOriginalRespues = contexto.Response.Body;
+
+            // 3. Cambio el stream por defecto del cuerpo
+            // de la respuesta por el MemoryStream creado
+            // para poder manipularlo
+            contexto.Response.Body = ms;
+
+            // 4. Esperamos a que el siguiente middleware
+            // devuelva la respuesta.
+            await siguiente(contexto);
+
+            // Salida del middleware
+
+            // 5. Nos movemos al principio del MemoryStream
+            // Para copiar el cuerpo de la respuesta
+            ms.Seek(0, SeekOrigin.Begin);
+
+            // 6. Leemos stream hasta el final y almacenamos
+            // el cuerpo de la respuesta obtenida
+            var respuesta = new StreamReader(ms).ReadToEnd();
+
+            // 5. Nos volvemos a posicionar al principio
+            // del MemoryStream para poder copiarlo al 
+            // cuerpo original de la respuesta
+            ms.Seek(0, SeekOrigin.Begin);
+
+            // 7. Copiamos el contenido del MemoryStream al
+            // stream original del cuerpo de la respuesta
+            await ms.CopyToAsync(cuerpoOriginalRespues);
+
+            // 8.Volvemos asignar el stream original al el cuerpo
+            // de la respuesta para que siga el flujo normal.
+            contexto.Response.Body = cuerpoOriginalRespues;
+
+            // 9. Escribimos en el log la respuesta obtenida
+            logger.LogInformation(respuesta);
+        }
+    }
+}
--- WEbApiAutores/Startup.cs
+++ WEbApiAutores/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 using WEbApiAutores.Controllers;
+using WEbApiAutores.Middlewares;
 using WEbApiAutores.Servicios;
 
 namespace WEbApiAutores
@@ -63,58 +64,9 @@ namespace WEbApiAutores
 
         public void Configure( IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
-            //app.Use(async (contexto, siguiente) => 
-            //{
-            //    //Informacion brindad por Raul Liz
-            //    // Entrada del middleware
- 
-            //    // 1. Creo un MemoryStream para poder manipular
-            //    // y copiarme el cuerpo de la respuesta.
-            //    // Esto se hace porque el stream del cuerpo de la
-            //    // respuesta no tiene permisos de lectura.
-            //    using var ms = new MemoryStream();
- 
-            //    // 2. Guardo la referencia del Stream donde se
-            //    // escribe el cuerpo de la respuesta
-            //    var cuerpoOriginalRespues = contexto.Response.Body;
- 
-            //    // 3. Cambio el stream por defecto del cuerpo
-            //    // de la respuesta por el MemoryStream creado
-            //    // para poder manipularlo
-            //    contexto.Response.Body = ms;
- 
-            //    // 4. Esperamos a que el siguiente middleware
-            //    // devuelva la respuesta.
-            //    await siguiente.Invoke();
- 
-            //    // Salida del middleware
- 
-            //    // 5. Nos movemos al principio del MemoryStream
-            //    // Para copiar el cuerpo de la respuesta
-            //    ms.Seek(0, SeekOrigin.Begin);
- 
-            //    // 6. Leemos stream hasta el final y almacenamos
-            //    // el cuerpo de la respuesta obtenida
-            //    var respuesta = new StreamReader(ms).ReadToEnd();
- 
-            //    // 5. Nos volvemos a posicionar al principio
-            //    // del MemoryStream para poder copiarlo al 
-            //    // cuerpo original de la respuesta
-            //    ms.Seek(0, SeekOrigin.Begin);
- 
-            //    // 7. Copiamos el contenido del MemoryStream al
-            //    // stream original del cuerpo de la respuesta
-            //    await ms.CopyToAsync(cuerpoOriginalRespues);
- 
-            //    // 8.Volvemos asignar el stream original al el cuerpo
-            //    // de la respuesta para que siga el flujo normal.
-            //    contexto.Response.Body = cuerpoOriginalRespues;
- 
-            //    // 9. Escribimos en el log la respuesta obtenida
-            //    logger.LogInformation(respuesta);
-            //});
-            
-            
+            //app.UseMiddleware<LoguearRespuestaHTTPMiddleware>();
+            app.UseLoguearRespuestaHTTP();
+
             //Si se quiere ir a una ruta especifica se utiliza Map
             app.Map("/ruta1", app => {
 
